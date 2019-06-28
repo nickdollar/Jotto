266bdad584a122a5a5ded1b3d03f04ee34daec88
Nicholas Homolka <ivelacc@gmail.com>
2019-06-28 07:53:38 -0600

    Extract enzyme setup to setupTests.js

---
+++ src/Congrats.test.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import {shallow} from 'enzyme'
+import checkPropTypes from 'check-prop-types'
+
+import { findByTestAttr, checkProps } from '../test/testUtils'
+import Congrats from './Congrats'
+
+
+const defaultProps = { success : false}
+
+/**
+ * Factory function to create a ShallowWrapper for the Congrats component.
+ * @function {} props 
+ * @param {object} props -Component props specific to this setuo
+ * @returns {ShallowWrapper} props 
+ */
+
+const setup = (props = {}) => {
+    const setupProps = {...defaultProps, ... props}
+    return shallow(<Congrats { ...setupProps } />)
+}
+
+
+test('renders without error', ()=>{
+    const wrapper = setup({success : false})
+    const component = findByTestAttr(wrapper, 'component-congrats')
+    expect(component.length).toBe(1)
+})
+
+test('renders no text when `success` prop is false', ()=>{
+    const wrapper = setup({ success: false})
+    const component = findByTestAttr(wrapper, 'component-congrats')
+    expect(component.text()).toBe('')
+})
+
+test('renders non-empty congrats message when `success` prop is true', ()=>{
+    const wrapper = setup({ success: true })
+    const message = findByTestAttr(wrapper, 'congrats-message')
+    expect(message.text().length).not.toBe(0)
+})
+
+
+test('does not throw warning expected props', ()=>{
+    const expectedProps = { success : false }
+    checkProps(Congrats, expectedProps)
+})
---
+++ test/testUtils.js
@@ -0,0 +1,19 @@
+import CheckPropTypes from 'check-prop-types'
+/**
+ * Return node(s) with the given data-test attribute
+ * @param {ShallowWrapper} wrapper - Enzyme shallow wrapper.
+ * @param {*} val - Value of data-test attribute for search.
+ * @returns {ShallowWrapper}
+ */
+
+export const findByTestAttr = (wrapper, val)=>{
+    return wrapper.find(`[data-test="${val}"]`)
+}
+
+export const checkProps = (component, conformingProps) =>{
+    const propError = CheckPropTypes(component.propTypes, 
+        conformingProps,
+        'prop',
+        component.name)
+        expect(propError).toBeUndefined()
+}
