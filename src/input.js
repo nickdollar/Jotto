4a2e635d37dec3b07516e83840101b0f9788bed7
Nicholas Homolka <ivelacc@gmail.com>
2019-07-04 20:46:37 -0600

    Add regus props test to input

--- src/input.js
+++ src/input.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
+import { guessWord } from './actions'
 
 class Input extends Component {
     
@@ -33,4 +34,4 @@ const mapStateToProps = ({success}) =>{
     return { success }
 }
 
-export default connect(mapStateToProps)(Input)
+export default connect(mapStateToProps, { guessWord })(Input)
